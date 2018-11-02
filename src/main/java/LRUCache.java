2540a9c270110c145982e3fcb247c3783e9007b4
Benjamin Ward <ward.programm3r@gmail.com>
2018-11-01 23:56:56 -0400

    Properly move node to front when it's already in the list

--- src/main/java/LRUCache.java
+++ src/main/java/LRUCache.java
@@ -1,5 +1,3 @@
-import sun.util.resources.cldr.be.CalendarData_be_BY;
-
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -9,98 +7,128 @@ import java.util.Objects;
  */
 public class LRUCache<T, U> implements Cache<T, U> {
 
-	private final HashMap<T, LinkedU> _map = new HashMap<>();
-	private final DataProvider<T, U> _baseProvider;
+    private final HashMap<T, Link> _map = new HashMap<>();
+    private final DataProvider<T, U> _baseProvider;
 
-	private final int _capacity;
+    private final int _capacity;
 
-    private LinkedU mostRecent, leastRecent;
+    private Link mostRecent, leastRecent;
 
     // The number of times that a get request misses the cache.
     private int _numMisses = 0;
 
-	/**
-	 * @param provider the data provider to consult for a cache miss
-	 * @param capacity the exact number of (key,value) pairs to store in the cache
-	 */
-	public LRUCache (DataProvider<T, U> provider, int capacity) {
-	    _baseProvider = provider;
-	    _capacity = capacity;
-	}
+    /**
+     * @param provider the data provider to consult for a cache miss
+     * @param capacity the exact number of (key,value) pairs to store in the cache
+     */
+    public LRUCache(DataProvider<T, U> provider, int capacity) {
+        _baseProvider = provider;
+        _capacity = capacity;
+    }
 
-	/**
-	 * Returns the value associated with the specified key.
-	 * @param key the key
-	 * @return the value associated with the key
-	 */
-	public U get (T key) {
-	    // If the cache map already contains the key, return it directly (this is _fast_).
-	    if(_map.containsKey(key))
-		    return _map.get(key).value;
+    /**
+     * Returns the value associated with the specified key.
+     *
+     * @param key the key
+     * @return the value associated with the key
+     */
+    public U get(T key) {
+        Link link;
 
-	    _numMisses++;
+        // If the cache map already contains the key, return it directly (this is _fast_).
+        if (_map.containsKey(key)) {
+            link = _map.get(key);
+            moveToFront(link);
+        } else {
+            _numMisses++;
 
-	    // Generate a new link to add to the linked list.
-	    LinkedU link = new LinkedU(key, _baseProvider.get(key));
-	    _map.put(key, link);
+            // Generate a new link to add to the linked list.
+            link = new Link(key, _baseProvider.get(key));
+            _map.put(key, link);
 
-	    // Insert the link into the linked list.
-	    if(leastRecent == null) {
-	        // The least recent item only needs updated initially and when we exceed the capacity.
-	        leastRecent = link;
-        }
-        // Save the newest link as the most recent.
-        if(mostRecent != null) {
-            link.previous = mostRecent;
-            mostRecent.next = link;
-        }
-        mostRecent = link;
+            // Insert the link into the linked list.
+            if (leastRecent == null) {
+                // The least recent item only needs updated initially and when we exceed the capacity.
+                leastRecent = link;
+            }
+            // Save the newest link as the most recent.
+            if (mostRecent != null) {
+                link.previous = mostRecent;
+                mostRecent.next = link;
+            }
+            mostRecent = link;
 
-	    // Remove an item from the linked list and map.
-        if(_map.size() > _capacity) {
-            // When we exceed the capacity, remove the least recent element from the map.
-            _map.remove(leastRecent.key);
-            // Then, make the next-least-recent element the least recent.
-            LinkedU next = leastRecent.next;
-            next.previous = null;
-            leastRecent = next;
+            // Remove an item from the linked list and map.
+            if (_map.size() > _capacity) {
+                // When we exceed the capacity, remove the least recent element from the map.
+                _map.remove(leastRecent.key);
+                // Then, make the next-least-recent element the least recent.
+                Link next = leastRecent.next;
+                next.previous = null;
+                leastRecent = next;
+            }
         }
 
         return link.value;
-	}
-
-	/**
-	 * Returns the number of cache misses since the object's instantiation.
-	 * @return the number of cache misses since the object's instantiation.
-	 */
-	public int getNumMisses () {
-		return _numMisses;
-	}
+    }
 
     /**
-     * Basis of a doubly-linked list containing a value of type {@code <U>} and key of type {@code <T>}.
+     * Returns the number of cache misses since the object's instantiation.
+     *
+     * @return the number of cache misses since the object's instantiation.
      */
-	class LinkedU {
-	    T key;
-	    U value;
+    public int getNumMisses() {
+        return _numMisses;
+    }
 
-	    LinkedU next;
-	    LinkedU previous;
+    /**
+     * Move the specified linked list node to the front of the list. Since this can only be called when a
+     * key is already present in the linked list, a large amount of null checking can be avoided for speed/simplicity.
+     * @param link The link to move to the front of the lists
+     */
+    private void moveToFront(Link link) {
+        if(mostRecent.equals(link))
+            return;
+        // Get the links in front of and behind the specified link and attach them together
+        final Link front = link.next;
+        final Link behind = link.previous;
+        front.previous = behind;
+        if(behind != null)
+            behind.next = front;
+        // Move the specified link to the front
+        link.next = null;
+        link.previous = mostRecent;
+        mostRecent.next = link;
+        mostRecent = link;
+    }
 
-	    private LinkedU(T key, U value) {
+    /**
+     * Basis of a naive doubly-linked list containing a value of type {@code <U>} and key of type {@code <T>}.
+     * This was used instead of the Java LinkedList class since it is less abstract, meaning that the list
+     * operations can be simplified for our specific use case.
+     */
+    class Link {
+        T key;
+        U value;
+
+        Link next;
+        Link previous;
+
+        private Link(T key, U value) {
             this.key = key;
-	        this.value = value;
+            this.value = value;
         }
 
-        private LinkedU getRoot() {
-	        if(next != null)
-	            return next.getRoot();
-	        return this;
+        // Unused
+        private Link getRoot() {
+            if (next != null)
+                return next.getRoot();
+            return this;
         }
 
         @Override
         public int hashCode() {
-	        return Objects.hash(key);
+            return Objects.hash(key);
         }
     }
 }
